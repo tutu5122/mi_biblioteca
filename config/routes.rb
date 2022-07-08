e81e7b9b80c873ba63ce27510c97d50a677ef075
tutu <mjperez5122@gmail.com>
2022-07-07 20:18:57 -0400

    Agrega vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   get 'books/index'
+  get 'books/new'
+  post 'books', to: 'books#create'
+
+  put 'books/:id', to: 'books#update'
+  patch 'books/:id', to: 'books#update'
+
+  # /books/1/edit
+  get 'books/:id/edit', to: 'books#edit', as: 'books_edit'
+  # /books/1
+  get 'books/:id', to: 'books#show', as: 'book'
 
 
   root 'books#index'
